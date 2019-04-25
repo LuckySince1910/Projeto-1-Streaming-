498196cea89edb49f0f6160c06c18549f96437b1
LuckySince1910 <50000844+LuckySince1910@users.noreply.github.com>
2019-04-25 20:30:58 -0300

    Tela de Login

---
+++ inicio.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Projeto Login</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css"> 
+</head>
+<body>
+<div id="corpo-form">
+	<h1>Entrar</h1>
+	<form method="POST" action="processa.php">
+		<input type="email" placeholder="Usuário">
+		<input type="password" placeholder="Senha">
+		<input type="submit" value="ACESSAR">
+		<a href="">Ainda não é inscrito?<strong>Cadastre-se!</strong></a>
+	</form>
+</div>
+</body>
+</html>
