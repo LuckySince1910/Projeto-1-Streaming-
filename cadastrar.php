2a0694980b9edb56e9b0b42d615019fcdf509808
LuckySince1910 <50000844+LuckySince1910@users.noreply.github.com>
2019-04-29 19:47:11 -0300

    Add files via upload

---
+++ cadastrar.php
@@ -0,0 +1,25 @@
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Projeto Login</title>
+	<link rel="stylesheet" type="text/css" href="estilocad.css"> 
+</head>
+<body>
+<div id="corpo-form-cad">
+	<h1>Cadastrar</h1>
+	<form method="POST">
+		<input type="email" nome="nome" placeholder="Email" maxlenght="40">
+		<input type="text" nome="usuario" placeholder="Usuário" maxlenght="30">
+		<input type="password" nome="senha" placeholder="Senha"maxlenght="15">
+		<input type="password" nome="confsenha" placeholder="Confirmar Senha"maxlenght="15">
+		<input type="submit" value="Cadastrar" style="color: white";>
+	</form>
+</div>
+<?php
+
+
+
+
+?>
+</body>
+</html>
